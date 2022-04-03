799ea5a43a44da17711c8e2508510240969acf77
kejlor <woj450@gmail.com>
2022-04-03 14:43:08 +0200

    Initial Commit

---
+++ iFlashCards/iFlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFlashCardsApp.swift
+//  iFlashCards
+//
+//  Created by Bartosz Wojtkowiak on 03/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct iFlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
